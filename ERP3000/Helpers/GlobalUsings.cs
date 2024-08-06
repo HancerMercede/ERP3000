bedf4193f88602050da074a0c50efdae52fd381d
Hancer <hancermercedes@gmail.com>
2024-08-06 11:50:32 -0400

    Adding globalusings class to add all the imports dependencies

---
+++ ERP3000/Helpers/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using ERP3000.Entities;
+global using ERP3000.Repository.Conctracts;
+global using ERP3000.Service.Contracts;
+global using Microsoft.AspNetCore.Mvc;
+global using ERP3000.DTOs;
+global using Mapster;
